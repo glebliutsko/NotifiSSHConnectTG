13695e492603c51f86a7274467c54f5602b16ffe
Gleb Liutsko <gleb290303@gmail.com>
2020-08-29 19:11:16 +0400

    Delete play sound

--- src/send_notify.sh
+++ src/send_notify.sh
@@ -7,5 +7,4 @@ TITLE="SSH Connection Established with \"$USER\""
 MESSAGE="$(echo $SSH_CONNECTION | sed 's/\(.*\) \(.*\) \(.*\) \(.*\)/from \1:\2 to \3:\4/')"
 
 export DISPLAY=:0
-notify-send -a "$NAMEAPP" -i $PATH_TO_ICON "$TITLE" "$MESSAGE" > /dev/null 2> /dev/null &
-cvlc --ignore-config --play-and-exit $PATH_TO_SOUND > /dev/null 2> /dev/null &
+notify-send -u critical -a "$NAMEAPP" -i $PATH_TO_ICON "$TITLE" "$MESSAGE" > /dev/null 2> /dev/null &
